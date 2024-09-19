933873fee9e87c77cdab9d3c3f3bc6d84921b063
Ray Devera <rayd808@yahoo.com>
2024-09-19 10:59:45 -0700

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,42 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    // prevents the whole page from refreshing after submitting our form
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all the fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false ) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
   return (
   <div className='min-h-screen mt-20'>
     <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -16,30 +50,37 @@ export default function SignUp() {
           <span>Brews'N'Books</span>
         </Link>
         <p className='text-sm mt-5'>
-          Explore and review local cafes and book-friendly spaces. Sign up to start finding your ideal study spot today!
+          Explore and review study spaces! Sign up with your email or with Google to start finding your ideal study spot today!
         </p>
       </div>
 
       {/* right side */}
       <div className='flex-1'>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <div className="">
             <Label value='Your username' />
-            <TextInput type='text' placeholder='JohnDoe123' id='username' />
+            <TextInput type='text' placeholder='JohnDoe123' id='username' onChange={handleChange} />
           </div>
 
           <div className="">
             <Label value='Your email' />
-            <TextInput type='email' placeholder='JohnDoe@domain.com' id='email' />
+            <TextInput type='email' placeholder='JohnDoe@domain.com' id='email' onChange={handleChange} />
           </div>
 
           <div className="">
             <Label value='Your password' />
-            <TextInput type='password' placeholder='Password' id='password' />
+            <TextInput type='password' placeholder='***********' id='password' onChange={handleChange} />
           </div>
           {/* in flowbite we need to specify the type. Without flowbite default is submit */}
-          <Button gradientDuoTone={'purpleToPink'} type='submit'>
-            Sign Up
+          <Button gradientDuoTone={'purpleToPink'} type='submit' disabled={loading}>
+            {
+              loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+                ) : ('Sign Up')
+              }
           </Button>
         </form>
         <div className="flex gap-2 text-sm mt-5">
@@ -48,6 +89,13 @@ export default function SignUp() {
             Sign In
           </Link>
         </div>
+        {
+          errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )
+        }
       </div>
     </div>
   </div>
